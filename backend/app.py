8fdfd67b3bb5751ef072fe4877538095437eb87a
mcV3rs <mcVers.pl@gmail.com>
2022-11-05 16:58:12 +0100

    Dodonie możliwości wgrywania plików poprzez żądanie POST lub stronę kliencką. Wgrywanie plików jest możliwe, lecz nie przechodzi ono automatycznych testów.

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,72 @@
+import imghdr
+import os
 import flask
+import logging
 import flask_cors
+from werkzeug.utils import secure_filename
+from werkzeug.exceptions import HTTPException
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = ['.txt', '.pdf', '.png', '.jpg', '.jpeg', '.gif']
 
 app = flask.Flask(__name__)
-
+app.config['UPLOAD_PATH'] = UPLOAD_FOLDER
+app.config['UPLOAD_EXTENSIONS'] = ALLOWED_EXTENSIONS
 cors = flask_cors.CORS(app)
+
+
+def validate_image(stream):
+    header = stream.read(512)  # 512 bytes should be enough for a header check
+    stream.seek(0)  # reset stream pointer
+    format = imghdr.what(None, header)
+    if not format:
+        return None
+    return '.' + (format if format != '.jpeg' else '.jpg')
+
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=400):
+        Exception.__init__(self)
+        self.error_message = message
+        if status_code is not None:
+            self.status_code = status_code
+
+    def to_dict(self):
+        return {
+            'message': self.error_message,
+            'error': True,
+        }
+
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    return error.to_dict(), error.status_code
 
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+
+@app.route('/file/upload', methods=['POST'])
+def upload():
+    uploaded_file = flask.request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+
+        if file_ext not in app.config['UPLOAD_EXTENSIONS'] or file_ext != validate_image(uploaded_file.stream):
+            raise InvalidUsage("Please provide valid image to upload", status_code=400)
+
+        path = os.path.join(app.config['UPLOAD_PATH'], filename)
+
+        uploaded_file.save(os.path.join(path))
+
+        return {"path": path}, 201
+    else:
+        raise InvalidUsage("Please provide file to upload", status_code=400)
 
 
 if __name__ == "__main__":
--- tests/backend_test.py
+++ tests/backend_test.py
@@ -1,10 +1,79 @@
-import pytest
+import io
 from backend.app import app
 
 
 def test_index():
-    tester = app.test_client()
-    response = tester.get("/", content_type="html/text")
+    # Setup
+    client = app.test_client()
 
+    # Execute
+    response = client.get("/",
+                          content_type="html/text")
+
+    # Assert
     assert response.status_code == 200
     assert response.data == b"Hello, World!"
+
+
+def test_upload_text_stream():
+    # Setup
+    file_name = "text_stream.txt"
+    client = app.test_client()
+    data = {
+        'file': (io.BytesIO(b"some text data"), file_name)
+    }
+
+    # Execute
+    response = client.post('/file/upload', data=data)
+
+    # Assert
+    assert response.status_code == 400
+    assert response.json["message"] == "Please provide valid image to upload"
+
+
+def test_upload_textfile():
+    # Setup
+    file = "files/text_file.txt"
+    client = app.test_client()
+    data = {
+        'file': (open(file, 'rb'), file)
+    }
+
+    # Execute
+    response = client.post('/file/upload', data=data)
+
+    # Assert
+    assert response.status_code == 400
+    assert response.json["message"] == "Please provide valid image to upload"
+
+
+def test_upload_image_file():
+    # Setup
+    filename = "files/1.png"
+    file = open(filename, 'rb')
+    client = app.test_client()
+    data = {
+        'file': (file, filename)
+    }
+
+    # Execute
+    response = client.post('/file/upload', data=data)
+
+    # Assert
+    assert response.status_code == 201
+
+
+def test_upload_image_stream():
+    # Setup
+    image_name = "fake_image_stream.jpg"
+    client = app.test_client()
+    data = {
+        'file': (io.BytesIO(b"random_bytes"), image_name)
+    }
+
+    # Execute
+    response = client.post('/file/upload', data=data)
+
+    # Assert
+    assert response.status_code == 201
+    assert response.json['file'] == image_name
