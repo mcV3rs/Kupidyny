f7fd8aa8f2e5d5c36da92368b793841cce5c23d7
mcV3rs <mcVers.pl@gmail.com>
2023-01-13 15:17:23 +0100

    Usunięcie plików testowych

--- test3.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open("project/instance/Karolina_Michał_wedding.csv", 'r') as f:
-    dict_reader = csv.DictReader(f)
-
-    list_of_dict = list(dict_reader)
-
-    print(list_of_dict[0]["wife"])
